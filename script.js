3ca8568be1416e72fd8e92a8ebf75271cdc5ada6
Jacey <jaceyopara@gmail.com>
2026-02-16 13:41:53 +0100

    added a function to update cards

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let timeData;
+const previousLabel ={
+    dailly: 'Yesterday',
+    weekly: 'Last Week',
+    monthly: 'Last Month'
+};
+
+let activeTimeframe = 'weekly';
+
+const buttons = document.querySelectorAll('.timeframe--buttons');
+const cards = document.querySelectorAll('.cards');
+
+function updateCards (data, timeframe) {
+    cards.forEach((card) => {
+        const activity = card.dataset.activity;
+        const activityData = data.find(item => item.title === activity);
+    });
+
+    cards.querySelectorAll('.current--time').textContent = activityData.timeframes[timeframe].current;
+    cards.querySelectorAll('.previous--time').textContent = activityData.timeframes[timeframe].previous;
+};
+
 fetch('data.json')
 .then((response) => {
     return response.json();
